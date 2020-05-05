f980348ffddc06801f9e1e09e8e38cd5937346ee
msayanece <msayanece@gmail.com>
2020-05-05 20:14:32 +0530

    update pom.xml and tests
    change junit4 to junit5 imports for the test classes. add jacoco plugin in pom.xml for code test coverage generation.

--- src/test/java/com/sayan/springboot/repository/CustomerRepositoryTest.java
+++ src/test/java/com/sayan/springboot/repository/CustomerRepositoryTest.java
@@ -2,7 +2,7 @@ package com.sayan.springboot.repository;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
-import org.junit.Test;
+import org.junit.jupiter.api.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
